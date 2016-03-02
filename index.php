98e3f6bb7aa10895a86aee37ba384180d6649708
Stan de Horn <Stan de Horn>
2016-03-02 13:00:55 +0100

    Updated styles to achieve paralax effect

--- index.php
+++ index.php
@@ -11,7 +11,49 @@
     </head>
     <body>
 
+        <div id="wrapper">
+            <!-- Paralax Header -->
+            <div class="header">
+                <div class="passPhoto">
+                    <h1>STAN DE HORN</h1>
+                </div>
+                <div class="foreGround"></div>
+                <div class="backGround"></div>
+                <div class="btnDownWrapper">
+                    <a href="#" class="btnDown">READ MORE <i class="fa fa-angle-down"></i></a>
+                </div>
+            </div>
 
+            <div class="jumbotron">
+                <div class="container">
+                    <h1 class="text-xs-center" id="uniqueStyle">Unique Style</h1>
+                </div>
+            </div>
+            <div class="container">
+                <hr>
+
+                <div class="col-xs-8 col-xs-push-2">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+            </div>
+
+
+            <!-- FAB Button -->
+            <div class="fabWrapper">
+                <div class="fab"><i class="fa fa-arrow-up"></i></div>
+            </div>
+
+        </div>
 
     </body>
+
+    <script src="js/jquery-1.12.1.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/Paralax.js"></script>
+
 </html>
