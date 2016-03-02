9347301ff43427c315da3d8f5b4132c3a56c4250
Stan de Horn <Stan de Horn>
2016-03-02 08:43:46 +0100

    Added index.php and declared the stylesheets

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/styles.css">
+        <title>Portfolio | Stan de Horn</title>
+    </head>
+    <body>
+
+
+
+    </body>
+</html>
